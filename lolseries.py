6ea379acf47c8a7200e92fd6e6e2214bfdf0712b
Hunter Boyd <hunterjsb@gmail.com>
2021-02-25 16:26:53 -0500

    added $record. added supporting get_winner and get_prediction methods to LCS object & LoLSeries Object. added predictions.json to .gitignore downloaded past_LCS.json as of 2/24/21

--- lolseries.py
+++ lolseries.py
@@ -1,13 +1,10 @@
-from requests import get
 import json
 from datetime import datetime
 
 # constants
-with open('./secrets.json') as s:
-    PANDASCORE_TOKEN = json.load(s)['PANDASCORE_TOKEN']
 main_slugs = {'LCS': 'league-of-legends-lcs', 'LEC': 'league-of-legends-lec'}
-LCS_ID = 4198
 LEC_ID = 4197
+LCS_START = datetime(2021, 2, 5)
 
 # GET NA WEEK - DECLARE END OF WEEKS AS wX_e
 now = datetime.now()
@@ -33,7 +30,8 @@ class LoLSeries:
         self.slug = main_slugs[region]
 
     def load_running_series(self):
-        """for getting local data about a current LoL series (id, slug, etc...)"""
+        """for getting local data about a current LoL series (id, slug, etc...)
+        :returns dict"""
         # OPEN THE LOCAL FILE
         with open('./assets/runningseries.json', 'r') as f:
             all_series = json.load(f)
@@ -43,48 +41,72 @@ class LoLSeries:
             if series['slug'] == self.slug + '-' + LoLSeries.SEASON:
                 return series
 
-    def load_team(self, team: str):
-        """for loading local data about a regional team"""
+    def load_team_by(self, key, value):
+        """for loading local data about a regional team
+        :returns dict"""
         # OPEN THE LOCAL FILE
         with open(f'./assets/teams_{self.region}.json', 'r') as f:
             all_teams = json.load(f)
 
         # RETURN CORRESPONDING ENTRY
         for c_team in all_teams:
-            if c_team['acronym'] == team:
+            if c_team[key] == value:
                 return c_team
 
-    def next_week_matches(self):
-        """ get next week's matches, returns a list of 15 strings formatted ' T1 vs T2' """
+    def get_matches(self, direction: str, w=lcs_week):
+        """ get next week's matches, returns a list of 15 strings formatted ' T1 vs T2'
+         :returns list"""
         # OPEN THE LOCAL FILE
-        with open(f"./assets/upcoming_{self.region}.json", 'r') as f:
+        with open(f"./assets/{direction}_{self.region}.json", 'r') as f:
             all_matches = json.load(f)
 
         # RETURN CORRESPONDING ENTRIES
         this_week = []
         for match in all_matches:
             week, name = match['name'].split(':')
-            if week == lcs_week:
+            if week == w:
+                name = name[1:]
                 this_week.append(name)
         return this_week
+
+    def get_winner_of_match(self, week: str, match_name: str):
+        # OPEN THE LOCAL FILE
+        with open(f"./assets/past_{self.region}.json", 'r') as f:
+            all_matches = json.load(f)
+
+        # MAKE THE INDEXING STRING
+        s = week + ': ' + match_name
+        winner = None
+
+        # GET THE CORRESPONDING WINNER
+        for match in all_matches:
+            if match['name'] == s:
+                results1, results2 = match['results'][0], match['results'][1]
+                if results1['score']:
+                    winner = self.load_team_by('id', results1['team_id'])
+                elif results2['score']:
+                    winner = self.load_team_by('id', results2['team_id'])
+
+        return winner['acronym']
 
 
 class LCS(LoLSeries):
     """The North American League Championship Series"""
+    ID = 4198
+
     def __init__(self):
         super().__init__('LCS')
-        self.t = 0  # to call in static method and remove a PEP8 lol
 
     def predict(self, predictions: list or tuple, author_id: str):
         """takes in list of strings as 2 or 3 letter team abbreviations & a discord author id as a str
-        then saves the predictions as json in ./assets"""
+        then saves the predictions as json in ./assets
+        :returns error msg or dict"""
         # DATA VALIDATION
         if len(predictions) != 15:  # 15 GPW
             return '`ERROR:` `Invalid prediction size`'
         else:
-            for i, match in enumerate(self.next_week_matches()):
+            for i, match in enumerate(self.get_matches('upcoming')):
                 t1, t2 = match.split(' vs ')
-                t1 = t1[1:]  # remove leading zero
                 if predictions[i].upper() not in [t1, t2]:
                     return f'`ERROR` `Bad prediction` {lcs_week} game {i+1}:{match}'
 
@@ -99,18 +121,49 @@ class LCS(LoLSeries):
             json.dump(all_predictions, f, indent=4)
         return all_predictions
 
-    def get_predictions(self, author_id: str):
-        """return a user's predictions... this is static :>"""
-        try:
-            with open('./assets/predictions.json', 'r') as f:
-                player_predictions = json.load(f)[author_id]
+    @staticmethod
+    def get_predictions(author_id: str):
+        """return a user's predictions... this is static :>
+        :returns dict"""
+        with open('./assets/predictions.json', 'r') as f:
+            raw = json.load(f)
+
+        # LOOK FOR THE PREDICTIONS
+        if author_id in raw:
+            player_predictions = raw[author_id]
             return player_predictions
-        except KeyError:
-            self.t += 1  # PEP8 staticmethod
-            return '`ERROR:` `NO PREDICTIONS`'
+        else:
+            return '`ERROR:` `NO USER`'
+
+    def get_record(self, author_id: str):
+        """ returns a dict of user predictions and stats
+        :returns [{'Week n': {'game': 't1 vs t2', 'winner': 'tn', 'correct': bool}}, {'Week n+1...}, ...]"""
+
+        # RETRIEVE PREDICTIONS
+        user_predictions = LCS.get_predictions(author_id)
+        ret = {}  # dict of arrays of 1's & 0's to return. 1's meaning a correct prediction. idx by week.
+
+        # CHECK AGAINST THE DUBS
+        for week in user_predictions:
+            ret_inner = []
+            if int(week[-1]) < int(lcs_week[-1]):
+
+                # GET USER PREDICTIONS AND ACTUAL RECORD
+                game_preds = user_predictions[week]
+                fri, sat, sun = game_preds[0:5], game_preds[5:10], game_preds[10:15]
+                outcomes = self.get_matches('past', week)
+                outcomes_fi, outcomes_sat, outcomes_sun = outcomes[0:5], outcomes[5:10], outcomes[10:15]
+
+                # COUNT WINS PER DAY AND ADD THE LIST TO THE RET DICT
+                for day, matches in zip([fri, sat, sun], [outcomes_fi, outcomes_sat, outcomes_sun]):
+                    for game in matches:
+                        winner = self.get_winner_of_match(week, game)
+                        oc = 1 if (winner.lower() in day) or (winner.upper() in day) else 0
+                        ret_inner.append((game, winner, oc))
+                ret[week] = ret_inner
+        return ret
 
 
 if __name__ == "__main__":
-    w3_LCS = LCS()
-    print(w3_LCS.predict(['C9', 'DIG', 'GG', 'TSM', '100', 'C9', 'IMT', '100', 'TL', 'GG', 'TSM', 'IMT', 'GG', 'C9',
-                          'EG'], 'admin'))
+    lee = LCS()
+    print(lee.get_record("213709745964580874"))
--- main.py
+++ main.py
@@ -11,33 +11,32 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.event  # readyuup
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(name=f'Welcome to week 420 of the LCS!'))
+    await bot.change_presence(activity=discord.Game(name=f'Welcome to {lcs_week} of the LCS!'))
     print('- R E A D Y -')
 
 
 @bot.command()
-async def lcs(ctx, *args):
+async def check(ctx, week=lcs_week):
     league = LCS()
-    author_id = str(ctx.author.id)
-    league.predict(args, author_id)
-    await ctx.send(lcs_week + ' predictions saved! Use $check to see them.')
 
-
-@bot.command()
-async def check(ctx):
-    league = LCS()
+    # GET AUTHOR
     author_name = ctx.author.name
     author_id = str(ctx.author.id)
 
     # INIT EMBED
-    pasta = discord.Embed(title=f'LCS {lcs_week}',
+    pasta = discord.Embed(title=f'LCS {week}',
                           description=f'*{author_name} predictions as of {datetime.now().strftime("%m/%d/%y")}*',
                           color=discord.Colour.gold())
     pasta.set_thumbnail(url="https://cdn.pandascore.co/images/league/image/4198/image.png")
 
-    # GET SCHEDULE AND PREDICTIONS
-    p = league.get_predictions(author_id)[lcs_week]
-    matches = league.next_week_matches()
+    # GET SCHEDULE AND PREDICTIONS OR PRINT ERR
+    p = league.get_predictions(author_id)
+    if (type(p) != dict) or (week not in p):  # ERROR CATCHING
+        await ctx.send('`ERROR 69:`    `NO PREDICTIONS`')
+        return
+    else:
+        p = p[week]
+    matches = league.get_matches('upcoming', week)
 
     # CONSTRUCT EMBED
     for i, match in enumerate(matches):
@@ -48,27 +47,65 @@ async def check(ctx):
 
 
 @bot.command()
-async def sched(ctx):
+async def sched(ctx, week=lcs_week):
+    league = LCS()
 
     # INIT EMBED
-    pasta = discord.Embed(title=f'LCS {lcs_week}',
-                          description=f'*schedule, {datetime.now().strftime("%m/%d/%y")}*',
+    pasta = discord.Embed(title=f'LCS {week}',
+                          description=f'*schedule as of {datetime.now().strftime("%m/%d/%y")}*',
                           color=discord.Colour.gold())
     pasta.set_thumbnail(url="https://cdn.pandascore.co/images/league/image/4198/image.png")
 
-    # GET SCHEDULE AND PREDICTIONS
-    league = LCS()
-    matches = league.next_week_matches()
-
-    # CONSTRUCT EMBED
+    # CONSTRUCT EMBED - LABEL MATCHES BY DAY
+    matches = league.get_matches('upcoming', week)
     for i, match in enumerate(matches):
-        if i < 5:
+        if i < 5:  # first five games are fri
             day = 'friday'
-        elif i < 10:
+        elif i < 10:  # next five are sat
             day = 'saturday'
-        else:
+        else:  # the last five are sun
             day = 'sunday'
         pasta.add_field(name=match, value=f"{day}, game {i+1}")
+
+    await ctx.send(embed=pasta)
+
+
+@bot.command()
+async def lcs(ctx, *args):
+    league = LCS()
+    # JUST SAVE THE STRING IN A FILE (VALIDATION IS DONE BY LCS OBJECT)
+
+    author_id = str(ctx.author.id)
+    league.predict(args, author_id)
+    await ctx.send(lcs_week + ' predictions saved! Use $check to see them.')
+
+
+@bot.command()
+async def record(ctx):
+    league = LCS()
+
+    # GET AUTHOR AND LOOK UP PREDICTIONS
+    author_id = str(ctx.author.id)
+    user_record = league.get_record(author_id)
+
+    # CONSTRUCT EMBED FOR EACH WEEK
+    pasta = discord.Embed()
+    pasta.set_thumbnail(url=ctx.author.avatar_url)
+    total_correct = 0  # counter variable
+    for oc_week in user_record:  # ocs = outcomes
+        ocs = user_record[oc_week]
+        pasta.title = f'{ctx.author.name} Predictions for LCS {oc_week}'
+
+        # oc[0] = GAME as STR 't1 vs t2'
+        # oc[1] = WINNER as STR 't1' or 't2'
+        # oc[3] = PREDICTION BOOL
+        for oc in ocs:
+            total_correct += oc[2]
+            pasta.add_field(name=oc[0], value=f'winner: **{oc[1]}**\ncorrect? {oc[2]}')
+
+        # CALCULATE THE WR
+        weekly_percent = round(100*total_correct/len(ocs), 2)
+        pasta.description = f'{weekly_percent}% CORRECT ({total_correct}/{len(ocs)})'
 
     await ctx.send(embed=pasta)
 
